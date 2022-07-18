3a40ab7c932096cc997d96eb97f7f93040bd5d7d
Vedant Kashyap <vedant15188@gmail.com>
2022-07-19 02:41:44 +0530

    Main logic for optional part done.

--- CircuitBreaker.py
+++ CircuitBreaker.py
@@ -1,11 +1,11 @@
-import imp
-import queue
 import requests
 from time import sleep
+from datetime import datetime
 import threading
 from http.server import HTTPServer
 from queue import Queue
 
+# Import custom HTTP server module
 import Server
 
 def startServer(webServer):
@@ -20,26 +20,54 @@ class CircuitBreaker:
         self.error_threshold = error_threshold
         self.time_window = time_window
         self.errorQueue = Queue(maxsize=error_threshold)
+        # Circuite state, 0 for closed, 1 for open
+        self.state = 0
 
     def do_request(self, url):
         try:
             response = self.http_client.get(url)
             print(response)
-            if(response.status_code == 200):
+            if(self.errorQueue.full()):
+                self.state = 1
+                print("Circuit Open!!")
+                print("Circuit is currenty in open state so no new requests to the service will be entertained!")
+                raise Exception()
+            elif(response.status_code == 200):
                 print("Successful response!")
                 self.errorQueue = Queue(maxsize=self.error_threshold)
             else:
                 print("Not a successful request!")
                 raise Exception()
         except:
-            self.errorQueue.put(1)
-            print("error count: ", self.errorQueue.qsize())
-            if(self.errorQueue.full()):
-                print("Circuit Open!")
-                print("Circuit is currenty in open state so no new requests to the service will be entertained!")
-                print("sleeping for ", self.time_window, " secs")
-                sleep(self.time_window)
+            print("Exception raised!")
+            errorEvent = {
+                "errorCode": response.status_code,
+                "timeStamp": datetime.now()
+            }
+
+            # Insert the error event if the queue is empty
+            if(self.errorQueue.empty()):
+                # print("Empty queue Case")
+                self.errorQueue.put(errorEvent)
+            # Exit the function if the circuit is open!
+            elif(self.state == 1):
+                # print("Circuit Open Case")
                 self.errorQueue = Queue(maxsize=self.error_threshold)
+                return
+            else:
+                # print("Appending error event to queue case")
+                queueCondition = True
+
+                # Keep dequeuing the error events till we're within the confines of the time window and then insert the event.
+                # errorQueue.queue[0] will get the first element of the queue without removing it
+                while(queueCondition):
+                    if(int((errorEvent["timeStamp"] - self.errorQueue.queue[0]["timeStamp"]).total_seconds()) > self.time_window):
+                        self.errorQueue.get()
+                    else:
+                        queueCondition = False
+                        self.errorQueue.put(errorEvent)
+
+            print("error count: ", self.errorQueue.qsize())
 
 if __name__ == "__main__":
     print("Hello WeTransfer!!\n-------------------------------\n")
@@ -55,24 +83,30 @@ if __name__ == "__main__":
 
         print("Server started http://%s:%s" % ("localhost", 8000))
 
-        timeOut = int(input("Input the timeout threshold (in seconds): "))
+        timeWindow = int(input("Input the time window (in seconds): "))
         errThreshold = int(input("Input the error threshold: "))
 
         N = int(input("Enter the total number of requests you want to send: "))
 
-        breaker = CircuitBreaker(requests, errThreshold, timeOut)
+        breaker = CircuitBreaker(requests, errThreshold, timeWindow)
 
-        for i in range(N):
+        for i in range(N):         
             print("Request no. ", i+1)
+            
             # Condition to simulate OPEN condition for CircuitBreaker
             if((i % (errThreshold*2)) < errThreshold):
-                print("Simulating error")
+                # print("Simulating error")
                 breaker.do_request("http://localhost:8000/error")
             else:
-                print("Simulating normal request")
+                # print("Simulating normal request")
                 breaker.do_request("http://localhost:8000/")
+
+            if(breaker.state == 1):
+                print("sleeping for ", timeWindow, " secs")
+                sleep(timeWindow)
+                breaker.state = 0
+            
             sleep(1)
     except Exception as e:
         print(e)
-        print("Failed to start the server")
         quit()
